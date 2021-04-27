1d82b3415f8f8c0941ed6e12109e8a40382fe68a
AlanCasella <alan.casella@hotmail.com.ar>
2021-04-27 07:11:44 -0300

    added one more project

--- src/assets/Slider.js
+++ src/assets/Slider.js
@@ -1,5 +1,6 @@
 import Eccommerce from "./Ecommerce.jpg";
 import Henry from "./HenryApp.jpg";
+import Task from "./List.jpg"
 
 const SliderData = [
     {
@@ -15,6 +16,12 @@ const SliderData = [
         link: "",
         repo: "https://github.com/AlanCasella/Proyecto-Final-ft07-04"
     },
+    {
+        image: Task,
+        visit: true,
+        link: "https://todo-tasks.vercel.app/",
+        repo: "https://github.com/AlanCasella/todo-tasks" 
+    }
 ]
 
 export default SliderData
